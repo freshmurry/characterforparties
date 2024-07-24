5dfdf38dd36d6134652c7c241bc41a5f77479976
Lawrence Murry <lawrencemurry@yahoo.com>
2024-07-24 01:11:10 -0500

    Updated code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
-  protected
+#   protected
+#     def update_resource(resource, params)
+#       resource.update_without_password(params)
+#     end
+# end
+
+protected
     def update_resource(resource, params)
-      resource.update_without_password(params)
+      if params[:image] # Replace :image with the actual attribute name if needed
+        resource.update(params)
+      else
+        resource.update_without_password(params)
+      end
     end
 end
