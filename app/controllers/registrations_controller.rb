43ec471556fa04a7df66610610d4c337946ab20b
Lawrence Murry <lawrencemurry@yahoo.com>
2024-09-07 02:32:14 -0500

    Trying to fix CSRF Token error. Error with registering/sigup in registrations_controller.rb file

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-  def update_resource(resource, params)
-    begin
-      resource.update_attributes(params)
-    rescue Aws::S3::Errors::AccessControlListNotSupported => e
-      Rails.logger.error "S3 ACL error: #{e.message}"
-      # Handle the error or notify the user
-    end
-  end
-  
   protected
     def update_resource(resource, params)
       resource.update_without_password(params)
